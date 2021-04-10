fca4b1d3acf10784beec2426daa858dc847b1068
Fernando Rafael Lorenzon <fernandolorenzon@gmail.com>
2021-04-09 23:48:34 -0300

    First commit

---
+++ CHD_Converter/FormMain.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ISO_To_CHD_Converter
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.buttonOpenSourceDir = new System.Windows.Forms.Button();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxIncludeSubfolders = new System.Windows.Forms.CheckBox();
+            this.checkedListBoxImages = new System.Windows.Forms.CheckedListBox();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonOutput = new System.Windows.Forms.Button();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.checkBoxAll = new System.Windows.Forms.CheckBox();
+            this.labelCurrent = new System.Windows.Forms.Label();
+            this.labelImage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonOpenSourceDir
+            // 
+            this.buttonOpenSourceDir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpenSourceDir.Image = ((System.Drawing.Image)(resources.GetObject("buttonOpenSourceDir.Image")));
+            this.buttonOpenSourceDir.Location = new System.Drawing.Point(416, 43);
+            this.buttonOpenSourceDir.Name = "buttonOpenSourceDir";
+            this.buttonOpenSourceDir.Size = new System.Drawing.Size(65, 23);
+            this.buttonOpenSourceDir.TabIndex = 0;
+            this.buttonOpenSourceDir.UseVisualStyleBackColor = true;
+            this.buttonOpenSourceDir.Click += new System.EventHandler(this.buttonOpenSourceDir_Click);
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSource.Location = new System.Drawing.Point(15, 45);
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(395, 20);
+            this.textBoxSource.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Folder containing Bin/Cue images";
+            // 
+            // checkBoxIncludeSubfolders
+            // 
+            this.checkBoxIncludeSubfolders.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxIncludeSubfolders.AutoSize = true;
+            this.checkBoxIncludeSubfolders.Checked = true;
+            this.checkBoxIncludeSubfolders.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxIncludeSubfolders.Location = new System.Drawing.Point(487, 47);
+            this.checkBoxIncludeSubfolders.Name = "checkBoxIncludeSubfolders";
+            this.checkBoxIncludeSubfolders.Size = new System.Drawing.Size(112, 17);
+            this.checkBoxIncludeSubfolders.TabIndex = 3;
+            this.checkBoxIncludeSubfolders.Text = "Include subfolders";
+            this.checkBoxIncludeSubfolders.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBoxImages
+            // 
+            this.checkedListBoxImages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBoxImages.FormattingEnabled = true;
+            this.checkedListBoxImages.Location = new System.Drawing.Point(15, 131);
+            this.checkedListBoxImages.Name = "checkedListBoxImages";
+            this.checkedListBoxImages.Size = new System.Drawing.Size(587, 289);
+            this.checkedListBoxImages.TabIndex = 4;
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(15, 428);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(71, 13);
+            this.labelOutput.TabIndex = 7;
+            this.labelOutput.Text = "Output Folder";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOutput.Location = new System.Drawing.Point(15, 445);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(398, 20);
+            this.textBoxOutput.TabIndex = 6;
+            // 
+            // buttonOutput
+            // 
+            this.buttonOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOutput.Image = ((System.Drawing.Image)(resources.GetObject("buttonOutput.Image")));
+            this.buttonOutput.Location = new System.Drawing.Point(419, 443);
+            this.buttonOutput.Name = "buttonOutput";
+            this.buttonOutput.Size = new System.Drawing.Size(65, 23);
+            this.buttonOutput.TabIndex = 5;
+            this.buttonOutput.UseVisualStyleBackColor = true;
+            this.buttonOutput.Click += new System.EventHandler(this.buttonOutput_Click);
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonConvert.Image = ((System.Drawing.Image)(resources.GetObject("buttonConvert.Image")));
+            this.buttonConvert.Location = new System.Drawing.Point(15, 500);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(117, 49);
+            this.buttonConvert.TabIndex = 8;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Image = ((System.Drawing.Image)(resources.GetObject("buttonImport.Image")));
+            this.buttonImport.Location = new System.Drawing.Point(15, 71);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(114, 54);
+            this.buttonImport.TabIndex = 9;
+            this.buttonImport.Text = "Import";
+            this.buttonImport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.buttonImport_Click);
+            // 
+            // checkBoxAll
+            // 
+            this.checkBoxAll.AutoSize = true;
+            this.checkBoxAll.Checked = true;
+            this.checkBoxAll.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAll.Location = new System.Drawing.Point(135, 108);
+            this.checkBoxAll.Name = "checkBoxAll";
+            this.checkBoxAll.Size = new System.Drawing.Size(71, 17);
+            this.checkBoxAll.TabIndex = 10;
+            this.checkBoxAll.Text = "Check All";
+            this.checkBoxAll.UseVisualStyleBackColor = true;
+            this.checkBoxAll.CheckedChanged += new System.EventHandler(this.checkBoxAll_CheckedChanged);
+            // 
+            // labelCurrent
+            // 
+            this.labelCurrent.AutoSize = true;
+            this.labelCurrent.Location = new System.Drawing.Point(77, 479);
+            this.labelCurrent.Name = "labelCurrent";
+            this.labelCurrent.Size = new System.Drawing.Size(52, 13);
+            this.labelCurrent.TabIndex = 11;
+            this.labelCurrent.Text = "No image";
+            // 
+            // labelImage
+            // 
+            this.labelImage.AutoSize = true;
+            this.labelImage.Location = new System.Drawing.Point(15, 479);
+            this.labelImage.Name = "labelImage";
+            this.labelImage.Size = new System.Drawing.Size(61, 13);
+            this.labelImage.TabIndex = 12;
+            this.labelImage.Text = "Converting:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 561);
+            this.Controls.Add(this.labelImage);
+            this.Controls.Add(this.labelCurrent);
+            this.Controls.Add(this.checkBoxAll);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonOutput);
+            this.Controls.Add(this.checkedListBoxImages);
+            this.Controls.Add(this.checkBoxIncludeSubfolders);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSource);
+            this.Controls.Add(this.buttonOpenSourceDir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "CHD Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenSourceDir;
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxIncludeSubfolders;
+        private System.Windows.Forms.CheckedListBox checkedListBoxImages;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonOutput;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Button buttonImport;
+        private System.Windows.Forms.CheckBox checkBoxAll;
+        private System.Windows.Forms.Label labelCurrent;
+        private System.Windows.Forms.Label labelImage;
+    }
+}
+
---
+++ CHD_Converter/FormMain.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ISO_To_CHD_Converter
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenSourceDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBoxSource.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void buttonOutput_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBoxOutput.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            if (textBoxSource.Text == "")
+            {
+                MessageBox.Show("Select bin/cue folder first");
+                return;
+            }
+
+            if (!Directory.Exists(textBoxSource.Text))
+            {
+                MessageBox.Show("Invalid bin/cue folder");
+                return;
+            }
+
+            checkedListBoxImages.Items.Clear();
+
+            var searchOptions = checkBoxIncludeSubfolders.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+
+            var files = Directory.GetFiles(textBoxSource.Text, "*.cue", searchOptions);
+
+            checkedListBoxImages.Items.AddRange(files);
+
+            for (int i = 0; i < checkedListBoxImages.Items.Count; i++)
+            {
+                checkedListBoxImages.SetItemChecked(i, true);
+            }
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            if (textBoxOutput.Text == "")
+            {
+                MessageBox.Show("Select an output folder first");
+                return;
+            }
+
+            if (!Directory.Exists(textBoxOutput.Text))
+            {
+                MessageBox.Show("Invalid output folder");
+                return;
+            }
+
+            var chdman = Environment.CurrentDirectory + "\\" + "chdman.exe";
+
+            if (!File.Exists(chdman))
+            {
+                MessageBox.Show("chdman.exe not found. Please put chdman.exe inside the CHD_Converter folder");
+                return;
+            }
+
+            int count = 0;
+
+            foreach (var item in checkedListBoxImages.CheckedItems)
+            {
+                var input = item.ToString();
+                var output = textBoxOutput.Text + input.Substring(input.LastIndexOf("\\"));
+                output = output.Replace(".cue", ".chd");
+                var command = "\"" + chdman + "\" createcd -i \"" + input + "\" -o \"" + output + "\"";
+
+                labelCurrent.Text = input.Substring(input.LastIndexOf("\\") + 1);
+
+                ExecuteCMD(command);
+                count++;
+            }
+
+            labelCurrent.Text = "Completed";
+            MessageBox.Show(count.ToString() + " Images converted");
+        }
+
+        private void Convert(string cuepath, string output)
+        {
+            var chdman = Environment.CurrentDirectory + "\\" + "chdman.exe";
+            var command = "\"" + chdman + "\" createcd -i \"" + cuepath + "\" -o \"" + output + "\"";
+
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(command);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+        }
+
+        private void ExecuteCMD(string comm)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(comm);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+        }
+
+        private void checkBoxAll_CheckedChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBoxImages.Items.Count; i++)
+            {
+                checkedListBoxImages.SetItemChecked(i, checkBoxAll.Checked);
+            }
+        }
+    }
+}
